03946e3dd0ca91fe720098e4096854841de0b8be
Patrick Jackson <PatrickSJackson@gmail.com>
2017-08-19 09:02:25 -0700

    Adding simple command line args and tracking of deviation.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 #[allow(dead_code)]
+
+
+#[macro_use]
+extern crate clap;
+use clap::{App, AppSettings, Arg};
+use std::cmp::{max, min};
+
 #[derive(Clone, Copy)]
 struct MyU128 {
     hi: u64,
@@ -93,6 +100,7 @@ struct Kolakoski {
     y: MyU128,
     idx: u64,
     count: u64,
+    deviation: f64,
 }
 
 impl Kolakoski {
@@ -102,6 +110,7 @@ impl Kolakoski {
             y: MyU128::new(0),
             idx: 1,
             count: 1,
+            deviation: 0.0,
         }
     }
 
@@ -111,6 +120,14 @@ impl Kolakoski {
         let f: MyU128 = self.y & !self.y.add_one();
         self.x ^= f;
         self.y = self.y.add_one() | (f & (self.x >> 1));
+        // Calculate deviation
+        let diff = max(self.count, self.idx / 2) - min(self.count, self.idx / 2);
+        let dev = diff as f64 / self.idx as f64;
+        self.deviation = if self.deviation < dev {
+            dev
+        } else {
+            self.deviation
+        };
     }
 
     /// Batch update the state
@@ -128,20 +145,48 @@ impl Kolakoski {
         let steps = index - self.idx;
         self.step(steps);
     }
+
+    pub fn reset_deviation(&mut self) {
+        self.deviation = 0.0;
+    }
 }
 
 fn main() {
+    let matches = App::new("kolakoski")
+        .global_settings(&[AppSettings::ColoredHelp])
+        .version(crate_version!())
+        .author("Patrick Jackson <PatrickSJackson@gmail.com>")
+        .about("counts the distribution of 1's in the Kolakoski Sequence")
+        .arg(
+            Arg::with_name("VALUE")
+                .help("Sets the value to find")
+                .required(false)
+                .default_value("1000000000")
+                .index(1)
+                .multiple(false),
+        )
+        .get_matches();
+
+    let target: u64 = matches
+        .value_of("VALUE")
+        .unwrap()
+        .parse::<u64>()
+        .expect("Must be a number.");
+
     let mut k = Kolakoski::new();
     let mut next_print = 10 as u64;
-    while k.idx < 1_000_000_000 {
+    while k.idx < target {
+        next_print = std::cmp::min(next_print, target);
         k.goto(next_print);
         println!(
-            "{}, {}, {}, {}",
+            "{}, {}, {}, {}, {}",
             k.idx,
             k.count,
+            k.deviation,
             k.x.bit_count(),
             k.y.bit_count()
         );
+        k.reset_deviation();
         next_print *= 10;
     }
 }
